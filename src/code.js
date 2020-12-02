4381da2cd1829930f76540f10f80be2a9ad5ab99
Mercuriver <mercuriver@gmail.com>
2020-12-02 13:31:12 +0900

    test(babel): Test extends/env config

--- src/code.js
+++
@@ -1,8 +0,0 @@
-// use @babel/preset-react
-const element = <div>babel test</div>; 
-// use @babel/plugin-transform-template-literals
-const text = `element type is ${element.type}`; 
-// use @babel/plugin-transform-arrow-functions
-const add = (a, b) => a + b;
-
-// command: npx babel src/code.js --presets=@babel/preset-react --plugins=@babel/plugin-transform-template-literals,@babel/plugin-transform-arrow-functions
